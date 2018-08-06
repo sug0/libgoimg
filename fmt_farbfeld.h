92a95a9b23ee9356a7b375621cd2b48bf0a0f847
sugoiuguu <sugoiuguu@tfwno.gf>
2018-08-06 21:29:04 +0100

    removed useless piece of code

--- fmt_farbfeld.h
+++ fmt_farbfeld.h
@@ -5,7 +5,5 @@
 
 extern int im_farbfeld_dec(Image_t *img, rfun_t rf, void *src);
 extern int im_farbfeld_enc(Image_t *img, wfun_t wf, void *dst);
-extern void im_farbfeld_at(Image_t *img, int x, int y, Color_t *dst);
-extern void im_farbfeld_set(Image_t *img, int x, int y, Color_t *src);
 
 #endif
